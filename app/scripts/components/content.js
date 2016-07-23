3763644f5681e8b7e6bc512a4182b3afc3feb03a
Mari Johannessen <mari.l.johannessen@gmail.com>
2016-07-23 15:20:14 -0500

    Added section component

--- app/scripts/components/content.js
+++ app/scripts/components/content.js
@@ -1,7 +1,11 @@
 const React = require('react');
+import Section from './section';
 
 const Content = React.createClass({
   render : function() {
+    return (
+      <Section />
+    )
   }
 });
 
