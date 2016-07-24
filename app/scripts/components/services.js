84b8fa98403f20a500987ef3e328fe3807c501c3
Mari Johannessen <mari.l.johannessen@gmail.com>
2016-07-24 17:08:33 -0500

    Started working on icons

--- app/scripts/components/services.js
+++ app/scripts/components/services.js
@@ -2,28 +2,35 @@ const React = require('react');
 
 // <Services />
 class Services extends React.Component {
+
   render() {
+    const residential = require('../../img/house.svg');
+    const investment = require('../../img/money-1.svg');
+    const land = require('../../img/mountain.svg');
+    const estate = require('../../img/houses.svg');
+    const staff = require('../../img/money.svg');
+
     return (
       <div className="services">
         <ul>
           <li>
-            <img className="icon" src="" />
+            <img className="icon" src={residential} />
             <h2>Residential</h2>
           </li>
           <li>
-            <img className="icon" src="" />
+            <img className="icon" src={investment} />
             <h2>Investment</h2>
           </li>
           <li>
-            <img className="icon" src="" />
+            <img className="icon" src={land} />
             <h2>Land</h2>
           </li>
           <li>
-            <img className="icon" src="" />
+            <img className="icon" src={estate} />
             <h2>Estate</h2>
           </li>
           <li>
-            <img className="icon" src="" />
+            <img className="icon" src={staff} />
             <h2>Full-time appraiser on permanent staff</h2>
           </li>
         </ul>
