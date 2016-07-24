8d101450365484f0b0dc1a73440f883f087cd5d7
TJ Egan <twegan@us.ibm.com>
2016-07-23 23:05:22 -0500

    more header styling

---
+++ app/scripts/components/hero.js
@@ -0,0 +1,15 @@
+const React = require('react');
+var bg = require('../../img/1.jpeg');
+
+// <Hero />
+class Hero extends React.Component {
+  render() {
+    return (
+      <div className="hero">
+
+      </div>
+    )
+  }
+};
+
+export default Hero;
