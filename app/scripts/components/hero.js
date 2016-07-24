7ba525e95530cdb0e573fb1f387a569ea98a8be7
Mari Johannessen <mari.l.johannessen@gmail.com>
2016-07-24 12:52:57 -0500

    Added the header in

--- app/scripts/components/hero.js
+++ app/scripts/components/hero.js
@@ -1,12 +1,13 @@
 const React = require('react');
 var bg = require('../../img/1.jpeg');
+import Form from './form';
 
 // <Hero />
 class Hero extends React.Component {
   render() {
     return (
       <div className="hero">
-
+        <Form />
       </div>
     )
   }
