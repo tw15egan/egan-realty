27d72d468d7d6b6b2e00b45a9be4cecb1fb2e8cf
Mari Johannessen <mari.l.johannessen@gmail.com>
2016-07-23 23:10:44 -0500

    Added more form stuff

--- app/scripts/components/form.js
+++ app/scripts/components/form.js
@@ -1,61 +1,85 @@
 const React = require('react');
+const $ = require('jQuery');
 
 // <Form />
 class Form extends React.Component {
-  super() {
-    return {
-      type: 'info',
-      message: ''
-    };
-  };
+  constructor() {
+    super();
+  }
 
   sendEmail(evt) {
     evt.preventDefault();
-    const formData = {
-      name: this.refs.name.value,
-      email: this.refs.email.value,
-      message: this.refs.message.value
-    };
+    const name = this.refs.name.value;
+    const email = this.refs.email.value;
+    const phone = this.refs.phone.value;
+    const message = this.refs.message.value;
+    const confirmation = document.querySelector('.form__confirmation');
+    confirmation.classList.add('show');
 
-    const xmlhttp = new XMLHttpRequest();
-    const _this = this;
-    xmlhttp.onreadystatechange = () => {
-      if (xmlhttp.readyState === 4) {
-        console.log(xmlhttp.responseText);
-        const response = JSON.parse(xmlhttp.responseText);
-        if (xmlhttp.status === 200 && response.status === 'OK') {
-          _this.setState({ type: 'success', message: 'We have recieved your message.'});
-        } else {
-          _this.setState({ type: 'danger', message: 'Sorry'});
-        }
-      }
-    };
-    xmlhttp.open('POST', 'send', true);
-    xmlhttp.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
-    xmlhttp.send(this.requestBuildQueryString(formData));
+    setTimeout(() => {
+      confirmation.classList.remove('show');
+    }, 4000);
+
+    $.ajax({
+      url: '//formspree.io/mari.l.johannessen@gmail.com',
+      method: 'POST',
+      data: {
+        name: name,
+        email: email,
+        phone: phone,
+        message: message
+      },
+      dataType: 'json'
+    });
   }
 
-  requestBuildQueryString(params) {
-    let queryString = [];
-    for (let property in params) {
-      if (params.hasOwnProperty(property)) {
-        queryString.push(encodeURIComponent(property) + '=' + encodeURIComponent(params[property]));
-      }
-      return queryString.join('&');
+  shrinkLabel(evt) {
+    const inputField = evt.target;
+    const inputFieldName = inputField.getAttribute('name');
+    const inputFieldLabel = document.querySelector('label[for="'+ inputFieldName + '"]');
+    inputFieldLabel.classList.add('active');
+    inputField.classList.add('active');
+
+    if (inputField.value === '') {
+      inputFieldLabel.classList.remove('active');
+      inputField.classList.remove('active');
     }
   }
 
   render() {
+
     return (
-      <form action="" className="form" onSubmit={this.sendEmail.bind(this)}>
-        <input type="text" ref="name" placeholder="Full Name" />
-        <input type="text" ref="email" placeholder="Email Address" />
-        <input type="number" ref="phone" placeholder="Phone Number" />
-        <textarea type="text" ref="message" placeholder="Your message"></textarea>
-        <button type="submit">Send Email</button>
+      <form className="form" onSubmit={this.sendEmail.bind(this)}>
+        <div className="form__confirmation">
+          <div>
+            <h1>Thank you for your message.</h1>
+            <h3>We will be in touch as soon as possible.</h3>
+          </div>
+        </div>
+        <fieldset className="form__container">
+          <input type="text" className="form__input" ref="name" name="name" onInput={this.shrinkLabel} required />
+          <label htmlFor="name" className="form__label">Full Name</label>
+        </fieldset>
+        <fieldset className="form__container">
+          <input type="email" className="form__input" ref="email" name="email" onInput={this.shrinkLabel} required />
+          <label htmlFor="email" className="form__label">Email Address</label>
+        </fieldset>
+        <fieldset className="form__container">
+          <input type="tel" className="form__input" ref="phone" name="phone" onInput={this.shrinkLabel} required />
+          <label htmlFor="phone" className="form__label">Phone Number</label>
+        </fieldset>
+        <fieldset className="form__container">
+          <textarea type="text" className="form__message" name="message" ref="message" onInput={this.shrinkLabel} required ></textarea>
+          <label htmlFor="message" className="form__label">Message</label>
+        </fieldset>
+        <input type="hidden" name="_next" value="/" />
+        <input type="text" name="_gotcha" className="form__hidden"/>
+        <fieldset className="form__container">
+          <button type="submit" className="form__button">Send Message</button>
+        </fieldset>
       </form>
     )
   }
-};
+}
 
 export default Form;
