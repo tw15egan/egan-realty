4c4fa61a7dbe9f001295e92fff6054a7e2673b88
TJ Egan <twegan@us.ibm.com>
2016-09-04 14:24:01 -0500

    make page responsive

--- app/scripts/components/areas.js
+++ app/scripts/components/areas.js
@@ -5,7 +5,7 @@ class Areas extends React.Component {
   render() {
     return (
       <div className="areas-banner">
-        
+
         <ul className="areas">
           <li>Albany</li>
           <li>Saratoga</li>
--- app/scripts/components/contact.js
+++ app/scripts/components/contact.js
@@ -1,47 +1,27 @@
 const React = require('react');
 import Section from './section';
 import Form from './form';
-import Map from './map';
+
+const map = require('./../../img/area.png');
 
 class Contact extends React.Component {
   constructor(props) {
     super(props);
-    
-    this.state = {
-      ready: false
-    };
-  }
-  
-  componentDidMount() {
-    console.log('mounted');
-    this.setState({
-      ready: true
-    });
   }
   
   render() {
-    // console.log(this.state.loading);
-    if (this.state.ready) {
-      return (
-        <div className="contact">
-          {/* <Map ready={this.state.ready} /> */}
-          <div className="contact__form">
-            <span>Get in touch</span>
-            <Form />
-          </div>
+  
+    return (
+      <div className="contact">
+        <div className="contact__map">
+          <img className="map" src={map} />
         </div>
-      )
-    } else {
-      return (
-        <div className="contact">
-          {/* <Map ready={this.state.ready} /> */}
-          <div className="contact__form">
-            <span>Get in touch</span>
-            <Form />
-          </div>
+        <div className="contact__form">
+          <span>Get in touch</span>
+          <Form />
         </div>
-      )
-    }
+      </div>
+    )
     
   }
 };
